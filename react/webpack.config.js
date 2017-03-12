95ef5ef5b3716f290afe867c4cee2c488c14ed09
Philip Cavaco <philipcavaco@gmail.com>
2017-03-12 12:51:06 +0000

    setup webpack dev server.

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -7,7 +7,7 @@ const PATHS = {
     dist: path.join(__dirname, 'dist'),
 };
 
-module.exports = {
+const commonConfig = {
     entry: PATHS.site,
     output: {
         filename: "[name].js",
@@ -17,6 +17,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Recipe Finder',
+            appMountId: 'site',
         }),
     ],
 
@@ -52,8 +53,64 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // },
+};
+
+function productionConfig() {
+    return commonConfig;
+}
+
+function developmentConfig() {
+    const config = {
+        devServer: {
+          // Enable history API fallback so HTML5 History API based
+          // routing works. This is a good default that comes
+          // in handy in more complicated setups.
+          historyApiFallback: true,
+          // Don't refresh if hot loading fails. If you want
+          // refresh behavior, set hot: true instead.
+          hotOnly: true,
+          // Display only errors to reduce the amount of output.
+          stats: 'errors-only',
+          // Parse host and port from env to allow customization.
+          //
+          // If you use Docker, Vagrant or Cloud9, set
+          // host: options.host || '0.0.0.0';
+          //
+          // 0.0.0.0 is available to all network devices
+          // unlike default `localhost`.
+          host: process.env.HOST, // Defaults to `localhost`
+          port: process.env.PORT, // Defaults to 8080
+          // Enable error/warning overlay
+          overlay: {
+            errors: true,
+            warnings: true,
+          },
+    }, plugins: [
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.NamedModulesPlugin(),
+        ],
+    };
+      return Object.assign(
+        {},
+        commonConfig,
+        config,
+        {
+          plugins: commonConfig.plugins.concat(config.plugins),
+        }
+    );
+}
+
+module.exports = function(env) {
+  console.log('env', env);
+  switch (env) {
+    case "development":
+        return developmentConfig();
+    case "production":
+    default:
+        return productionConfig();
+  }
 };
